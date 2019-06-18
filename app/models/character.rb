9ccce1e907371b6779e900a542e0e3fbf033b2a1
github username <github email address>
2019-06-18 09:36:39 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+  	"#{name} always says: #{catchphrase}"
+  end
+
+  def build_show(show_attrs)
+  	new_show = Show.create(show_attrs)
+  	new_show.characters << self
+  	new_show
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+	has_many :characters
+	has_many :actors, through: :characters
+	belongs_to :network
+
+	def build_network(network_attrs)
+		new_network = Network.create(network_attrs)
+		self.network_id = new_network.id
+	end
 end
